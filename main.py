8a0e02505abf0ccabd80c256aca39d6c63737cfb
ksg-dev <ksg.dev.data@gmail.com>
2024-06-19 12:35:40 -0500

    add sheety api

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import requests
+from datetime import datetime
 
+# NutritionIX
 APP_ID = "d78dcd60"
 API_KEY = "1c5f2b1a046b8ef81c40f03e7eb746b2	"
 HOST_DOMAIN = "https://trackapi.nutritionix.com"
@@ -11,6 +13,7 @@ headers = {
     "x-app-key": API_KEY,
 }
 
+
 user_input = input("Enter your exercise: ")
 
 exercise_config = {
@@ -18,5 +21,42 @@ exercise_config = {
 }
 
 response = requests.post(url=NL_ENDPOINT, json=exercise_config, headers=headers)
+data = response.json()
 
-print(response.text)
+exercise = data["exercises"][0]["name"].title()
+duration = int(data["exercises"][0]["duration_min"])
+calories = int(data["exercises"][0]["nf_calories"])
+# print(data)
+# print(exercise)
+# print(duration)
+
+
+
+
+# Sheety
+SHEETY_ENDPOINT = "https://api.sheety.co/e938eee05710f2891def63457d60664d/myWorkouts/workouts"
+
+# sheety_response = requests.get(url=SHEETY_ENDPOINT)
+# print(sheety_response.text)
+
+
+
+today = datetime.now()
+logdate = today.strftime("%d/%m/%Y")
+logtime = today.strftime("%H:%M:%S")
+
+# print(today)
+# print(logdate)
+# print(logtime)
+
+new_row = {
+    "workout": {
+        "date": logdate,
+        "time": logtime,
+        "exercise": exercise,
+        "duration": duration,
+        'calories': calories,
+    }
+}
+
+add_row = requests.post(url=SHEETY_ENDPOINT, json=new_row, headers=headers)
