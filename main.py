e015c11eceae29fc01a546d3800a9cf8e4fcbe62
ksg-dev <ksg.dev.data@gmail.com>
2024-06-19 12:59:05 -0500

    take user input update sheet

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import requests
 from datetime import datetime
+from dotenv import load_dotenv
+import os
 
+load_dotenv(".env")
 # NutritionIX
-APP_ID = "d78dcd60"
-API_KEY = "1c5f2b1a046b8ef81c40f03e7eb746b2	"
-HOST_DOMAIN = "https://trackapi.nutritionix.com"
-NATURAL_LANG = "/v2/natural/exercise"
-NL_ENDPOINT = f"{HOST_DOMAIN}{NATURAL_LANG}"
+APP_ID = os.getenv("APP_ID")
+API_KEY = os.getenv("API_KEY")
+NL_ENDPOINT = os.getenv("NL_ENDPOINT")
+
+# Sheety
+SHEETY_ENDPOINT = os.getenv("SHEETY_ENDPOINT")
+
 
 headers = {
     "x-app-id": APP_ID,
@@ -26,28 +31,10 @@ data = response.json()
 exercise = data["exercises"][0]["name"].title()
 duration = int(data["exercises"][0]["duration_min"])
 calories = int(data["exercises"][0]["nf_calories"])
-# print(data)
-# print(exercise)
-# print(duration)
-
-
-
-
-# Sheety
-SHEETY_ENDPOINT = "https://api.sheety.co/e938eee05710f2891def63457d60664d/myWorkouts/workouts"
-
-# sheety_response = requests.get(url=SHEETY_ENDPOINT)
-# print(sheety_response.text)
-
-
 
 today = datetime.now()
 logdate = today.strftime("%d/%m/%Y")
 logtime = today.strftime("%H:%M:%S")
-
-# print(today)
-# print(logdate)
-# print(logtime)
 
 new_row = {
     "workout": {
